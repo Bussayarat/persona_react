b4361906b3a071d2b62a5b4ff2b721ad570dde42
ladybee <bussayarat_@hotmail.com>
2018-08-09 18:37:30 +0700

    commit TH 27 5.5cp

--- test.py
+++ test.py
@@ -2,15 +2,31 @@
 from collections import Counter
 from pythainlp.tokenize import word_tokenize, WhitespaceTokenizer, sent_tokenize
 
-# for radar chart
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.path import Path
-from matplotlib.spines import Spine
-from matplotlib.projections.polar import PolarAxes
-from matplotlib.projections import register_projection
+# for chart
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from matplotlib.path import Path
+# from matplotlib.spines import Spine
+# from matplotlib.projections.polar import PolarAxes
+# from matplotlib.projections import register_projection
 
-# just for debug code
+# cpltd
+# def set2initial_value():
+
+# def set_value(count):
+#     for key,value in count.items():
+#         print(key)
+#         print(value)
+#         if not (key is None):
+#             exec("%s = %d" % (key,value))
+
+
+def search_dict(dict, lookup):
+    for key, value in dict.items():
+        for word in value:
+            if lookup in word:
+                return key
+# just for debug
 emo_dict_2 = {
 'anger' : ['แย่','โคตร'],
 'sadness' : ['แต่ละวัน','แต่'],
@@ -23,31 +39,16 @@ example = 'แต่ละวันดูแย่มากโคตรแย่
 example = word_tokenize(example, engine='newmm')
 len_word = len(example)
 sum_emo = []
+
 for count in range (0, len_word):
     word = example[count]
-    print(count)
-    emotion = search_emotoins(emo_dict_2, word)
-    print(emotion)
+    emotion = search_dict(emo_dict_2, word)
     sum_emo.append(emotion)
-    print(word)
-
-print (sum_emo) # ['sadness', 'joy', None, 'anger', 'anger', None, 'anger']
 count = Counter(sum_emo)
-print(count) #debug
-print('anger = ',dict(count)['anger'])
-# ใน 1% จะมีผลทางอารมณ์แต่ละด้านเป็นเท่าไหร่
-# 100% คือทั้งหมดรวมกันเป็นคะแนนดิบ  5 6 7 8 ...
-# ถ้ามี anger 5 sad 7 happy 6 trust 1 , 1% = 19 --> 5/19 = ?%
 
 
-def search_emotoins(dict, lookup):
-    for key, value in dict.items():
-        for word in value:
-            if lookup in word:
-                return key
-
-def cal_emo(emo_list):
-    if anger = 0, fear =0 , glade = 0 :
-        print (mood)
-# persona_react
-# persona_react
+for key,value in count.items():
+    print(key)
+    print(value)
+    if not (key is None):
+        exec("%s = %d" % (key,value))
